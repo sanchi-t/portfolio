40ade04afc6bb284c457351a28287d4e80a40e75
sanchi-t <ssharmas10662@gmail.com>
2023-08-22 19:16:43 +0530

    Updates

---
+++ precache-manifest.278d515e9fbc1df43dc8dfd581214c3b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9071b2276445529a2eb01e999de018b",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "268f644af41a7e585d72",
+    "url": "/portfolio/static/css/2.3eb2a443.chunk.css"
+  },
+  {
+    "revision": "22b48b26ee6b8707299e",
+    "url": "/portfolio/static/css/main.f3060e22.chunk.css"
+  },
+  {
+    "revision": "268f644af41a7e585d72",
+    "url": "/portfolio/static/js/2.ee3abb3c.chunk.js"
+  },
+  {
+    "revision": "334a3166bc9976dda67650e8c6f88652",
+    "url": "/portfolio/static/js/2.ee3abb3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22b48b26ee6b8707299e",
+    "url": "/portfolio/static/js/main.72ef4319.chunk.js"
+  },
+  {
+    "revision": "0d4bf47b70941af9967b",
+    "url": "/portfolio/static/js/runtime-main.8940859c.js"
+  }
+]);
