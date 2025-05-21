156f4c490b096e1bbea20d7d22734de0b2f894fa
Sanchit Sharma <ssharmas10662@gmail.com>
2025-05-21 07:02:40 +0530

    Updates

---
+++ precache-manifest.a5327e37925027edad14ccd7e4aeeb36.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1abbdc172328cf4bc07ca51d71c25de8",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "4d04936fe02c382ccc4b",
+    "url": "/portfolio/static/css/2.6f85094a.chunk.css"
+  },
+  {
+    "revision": "c298dac598131019466d",
+    "url": "/portfolio/static/css/main.a7c5adb5.chunk.css"
+  },
+  {
+    "revision": "4d04936fe02c382ccc4b",
+    "url": "/portfolio/static/js/2.c95fd449.chunk.js"
+  },
+  {
+    "revision": "03c537d73dd0c01f3ff35bbde2aec257",
+    "url": "/portfolio/static/js/2.c95fd449.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c298dac598131019466d",
+    "url": "/portfolio/static/js/main.e9090a3f.chunk.js"
+  },
+  {
+    "revision": "0d4bf47b70941af9967b",
+    "url": "/portfolio/static/js/runtime-main.8940859c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.de9a92bc0f7fed20e48d29e9c12c7c2e.js"
+  "/portfolio/precache-manifest.a5327e37925027edad14ccd7e4aeeb36.js"
 );
 
 self.addEventListener('message', (event) => {
